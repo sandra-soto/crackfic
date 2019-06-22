05206ccf17b99b2d8d74563169aac175b63e6509
whydude <sandra909@live.com>
2019-06-22 16:59:28 -0700

    documentation & basic set up

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask
+import flask
 from flask import request, redirect, url_for
+app = flask.Flask(__name__)
 
-app = Flask(__name__)
 
+@app.route('/') # this is the main page
+def main():
+    return flask.render_template('main.html')
+    #^^^^ this will render the main.html file
+    #in the templates folder
 
-@app.route('/')
-def hello_world():
-    return 'test1'
+@app.route('/search',methods = ['POST']) # this is an "html form"
+def search():
+    if request.method == 'POST':
+        search_category = request.form['search_category']
+        return redirect(url_for('success', name = search_category))
+
+@app.route('/success/<name>') # this will redirect us to a successful search
+def success(name): 
+   return 'This category is ' + str(len(name))+ 'characters long' + 'welcome %s' % name 
 
 
 if __name__ == '__main__':
-    app.debug=True
+    app.debug=True # this will give us an error message when the app crashes
     app.run()
