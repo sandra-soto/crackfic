db000e747f93732df0b54c102bd146b7cc43ce3c
alexkumar520 <43835304+alexkumar520@users.noreply.github.com>
2019-06-27 22:10:43 -0700

    Add files via upload

--- subcategory.py
+++ subcategory.py
@@ -1,4 +1,6 @@
 ##decodertest
+from bs4 import BeautifulSoup
+import requests
 
 import urllib.request
 
@@ -9,19 +11,33 @@ def main_category(category: str):
     '''use this func to generate the url for category that gives the different subcategories if applicable'''
     return official_url + category
 
-def generate_subs(url: str):
-    '''returns a list of subcategories'''
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    response.close()
-    text = data.decode('utf-8')
-    x = text.split()
-    categoryset = list()
-    for i in x:
-        if "href" in i and "/anime/" in i:
-            if "</li>" not in i:
-                clean = i[13:].index("/")
-                categoryset.append(i[13:][:clean])
 
-    return (categoryset)
+def generate_subs(url: str, category: str):
+    '''return list of subcategories. **UPDATED**'''
+    #subcats = []
+    page = requests.get(url)
+    #text = page.content.decode(encoding = 'utf-8')
+    #official = text.splitlines()
+    #print(official)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    subcats = soup.find_all("a")
+    newfilter = []
+    for i in subcats:
+        title = i.get('title')
+        newfilter.append(title)
+    finalfilter = []
+    for i in newfilter:
+        if i != None:
+            finalfilter.append(i)
+    reallyfinal = []
+    for i in finalfilter:
+        if "\\'" in str(i):
+            x = i.replace("\\", '')
+            reallyfinal.append(x)
+        else:
+            reallyfinal.append(str(i))
+            
+    
+    print(reallyfinal)
         
+
