c1fc5febf1cfac02e852caf83de2338e18d53855
whydude <sandra909@live.com>
2019-07-05 01:58:42 -0700

    Revert "timeout fix attempt 1"
    This reverts commit 3b862b8f23da82b9051d857fb5f8af65597576a7.

--- app.py
+++ app.py
@@ -35,9 +35,6 @@ def success(name):
 
 @app.route('/madlib/<fandom>')
 def madlib(fandom):
-    def generate():
-      for i in range(10):
-        yield "<br/>"
     return "Welcome  to the madlib page for " + fandom + '\n' + sc.random_story_in_page('anime', fandom)
 
 if __name__ == '__main__':
