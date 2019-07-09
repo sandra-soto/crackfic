5a0f6d1482973cf1519bf80cfd601263aa4e432f
whydude <sandra909@live.com>
2019-07-08 22:24:57 -0700

    fixed global var issue

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import storyscrape as sc
 import flask
 import random
-from flask import request, redirect, url_for, jsonify, render_template
+from flask import request, redirect, url_for, jsonify, render_template, session
 app = flask.Flask(__name__)
+app.secret_key = 'huh'
 
 import subcateg_list
-fndm = None
 
 @app.route('/') # this is the main page
 def main():
@@ -32,16 +32,16 @@ def success(name):
         fandom_selection = fandom_selection.replace('/',' ')
         return redirect(url_for('madlib', fandom = fandom_selection))
 
-@app.route('/madlib/<fandom>', methods = ['GET','POST'])
-def madlib(fandom):
+@app.route('/madlib/<fandom>', methods = ['GET', 'POST'])
+def madlib(fandom=None):
     if request.method == 'GET':
-        global fndm
-        fndm = fandom
+        session['fandom'] = fandom
         randnum = random.randint(1,20)
         return render_template('madlib.html',rand = randnum) #creates random number of input boxes from 1-20
     if request.method == 'POST':
+        fandom = session['fandom']
         word_list = request.form.getlist('input_text[]') #lowkey dont know if this works but lmao
-        return redirect(url_for('testinputs', fandom=fndm, words=word_list))
+        return redirect(url_for('testinputs', fandom=fandom, words=word_list))
         #return "Welcome  to the madlib page for " + fandom + '\n' + sc.random_story_in_page(fandom) #sc.correct_subcategory_link(fandom)
     
 @app.route('/why/<fandom>/<words>')
